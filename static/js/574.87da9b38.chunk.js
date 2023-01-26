596918d610ca81ff49941eb23ccf04447de0f8d9
Ranjith Niren <96245879+prejitkichu@users.noreply.github.com>
2023-01-27 03:25:40 +0530

    phase 1
    phase 1 commits

---
+++ static/js/574.87da9b38.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_yarn_route=self.webpackChunkreact_yarn_route||[]).push([[574],{8574:function(e,r,n){n.r(r);var t=n(184);r.default=function(){return(0,t.jsx)("h1",{children:"NO PAGE FOUND"})}}}]);
+//# sourceMappingURL=574.87da9b38.chunk.js.map
